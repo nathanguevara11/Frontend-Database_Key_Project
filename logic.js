580d89f8f747ea48af9bb459ad0b24eecb908f2e
nathanguevara11 <nathangeagle@hotmail.com>
2026-01-08 03:19:17 +0000

    P10 final debugging for now

--- logic.js
+++ logic.js
@@ -309,8 +309,9 @@ function resetPage() {
 
     key_dropdown.style.display = "none"; 
     building_dropdown.style.display = "none"; 
-    // confirm_key_btn.style.display = "inline-block";
-    // confirm_building_btn.style.display = "inline-block";
+    confirm_key_btn.style.display = "inline-block";
+    confirm_building_btn.style.display = "inline-block";
+    confirm_building_btn.hidden = true; 
 
     checked_in_mess.style.display = 'none';
     checked_in_mess_2.style.display = 'none';
